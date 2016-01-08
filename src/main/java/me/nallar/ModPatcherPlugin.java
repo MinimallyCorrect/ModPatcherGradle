02d83a6189f082ff99fceaf719f3729bc1517b16
Ross Allan <ross@nallar.me>
2016-01-08 00:24:22 +0000

    Add binary/source processing tasks

--- src/main/java/me/nallar/ModPatcherPlugin.java
+++ src/main/java/me/nallar/ModPatcherPlugin.java
@@ -1,16 +1,44 @@
 package me.nallar;
 
+import lombok.Data;
+import lombok.val;
 import me.nallar.modpatcher.tasks.TaskExtractGeneratedSources;
 import me.nallar.modpatcher.tasks.TaskGenerateInheritanceHierarchy;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 public class ModPatcherPlugin implements Plugin<Project> {
+	public static ModPatcherGradleExtension extension = new ModPatcherGradleExtension();
+	public static String BINARY_PROCESSING_TASK = "processMcBin";
+	public static String SRC_PROCESSING_TASK = "processMcSrc";
+
 	@Override
 	public void apply(Project project) {
 		project.getPlugins().apply("net.minecraftforge.gradle.forge");
 
-		project.getTasks().create("generateInheritanceHierarchy", TaskGenerateInheritanceHierarchy.class);
-		project.getTasks().create("extractGeneratedSources", TaskExtractGeneratedSources.class);
+		project.getExtensions().add("modpatcher", extension);
+
+		val tasks = project.getTasks();
+		tasks.create(BINARY_PROCESSING_TASK, TaskGenerateInheritanceHierarchy.class);
+		tasks.create(SRC_PROCESSING_TASK, TaskExtractGeneratedSources.class);
+
+		project.afterEvaluate(this::afterEvaluate);
+	}
+
+	public void afterEvaluate(Project project) {
+		val tasks = project.getTasks();
+
+		// generateInheritanceHierarchy required for setupCiWorkspace. Runs after deobfMcMCP
+		tasks.getByName(BINARY_PROCESSING_TASK).mustRunAfter("deobfMcMCP");
+		tasks.getByName("setupCiWorkspace").dependsOn(BINARY_PROCESSING_TASK);
+
+		tasks.getByName(SRC_PROCESSING_TASK).mustRunAfter("remapMcSources");
+		tasks.getByName("recompileMc").dependsOn(SRC_PROCESSING_TASK);
+	}
+
+	@Data
+	public static class ModPatcherGradleExtension {
+		public boolean extractGeneratedSources = false;
+		public boolean generateInheritanceHierarchy = false;
 	}
 }
